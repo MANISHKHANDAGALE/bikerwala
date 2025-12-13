8c849e0f618f885390cc28ebf91cd12559c4b93c
MANISH <khandagalemanish21@gmail.com>
2025-12-13 17:23:01 +0530

    Manish All Section Done

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component, input } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 import {MatIconButton ,MatButtonModule} from '@angular/material/button';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { BIKE_SEARCH_DATA } from '../../dummy-data';
 @Component({
   selector: 'app-search-bar',
   imports: [FontAwesomeModule, MatIconButton, CommonModule],
@@ -10,7 +11,28 @@ import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
   styleUrl: './search-bar.component.css'
 })
 export class SearchBarComponent {
+
  faMagnifyingGlass=faMagnifyingGlass
+ query = ''
+ suggestions = signal<string[]>([])
  placeholder = input<string>('Search')
  width = input<string>('100%')
+ private timer! : ReturnType<typeof setTimeout>
+ onSearch(val:string){
+  this.query = val
+  clearTimeout(this.timer)
+  this.timer = setTimeout(()=>{
+    console.log("debounce value :",this.query)
+    const term = this.query.toLowerCase();
+    const data = [
+      ...BIKE_SEARCH_DATA.bikes,
+      ...BIKE_SEARCH_DATA.brands,
+      ...BIKE_SEARCH_DATA.categories,
+      ...BIKE_SEARCH_DATA.engineCCRanges,
+      ...BIKE_SEARCH_DATA.priceRanges
+    ]
+    this.suggestions.set(data.filter((item)=> item.toLowerCase().includes(term)).slice(0,8))
+    console.log(this.suggestions())
+  },300)
+ }
 }
---
+++ src/app/dummy-data.ts
@@ -0,0 +1,68 @@
+// src/app/core/dummy-data/bike-search.dummy.ts
+
+export const BIKE_SEARCH_DATA = {
+  brands: [
+    "Royal Enfield",
+    "Yamaha",
+    "KTM",
+    "Honda",
+    "TVS",
+    "Bajaj",
+    "Hero",
+    "Suzuki",
+    "BMW",
+    "Kawasaki"
+  ],
+
+  bikes: [
+    "Royal Enfield Classic 350",
+    "Royal Enfield Bullet 350",
+    "Royal Enfield Hunter 350",
+    "Yamaha R15 V4",
+    "Yamaha MT 15",
+    "Yamaha FZ S",
+    "KTM Duke 200",
+    "KTM Duke 390",
+    "KTM RC 390",
+    "Honda CB350",
+    "Honda CB350RS",
+    "Honda Hornet 2.0",
+    "TVS Apache RTR 160 4V",
+    "TVS Apache RTR 200 4V",
+    "Bajaj Pulsar NS200",
+    "Bajaj Pulsar N160",
+    "Hero Xpulse 200 4V",
+    "Hero Splendor Plus",
+    "Suzuki Gixxer SF",
+    "BMW G 310 R",
+    "Kawasaki Ninja 300"
+  ],
+
+  categories: [
+    "Cruiser",
+    "Sports",
+    "Naked",
+    "Street",
+    "Adventure",
+    "Scooter",
+    "Scrambler",
+    "Commuter"
+  ],
+
+  priceRanges: [
+    "Under 1 Lakh",
+    "1 - 1.5 Lakh",
+    "1.5 - 2 Lakh",
+    "2 - 3 Lakh",
+    "Above 3 Lakh"
+  ],
+
+  engineCCRanges: [
+    "Below 125cc",
+    "125cc - 160cc",
+    "160cc - 200cc",
+    "200cc - 300cc",
+    "300cc - 500cc",
+    "Above 500cc"
+  ]
+};
