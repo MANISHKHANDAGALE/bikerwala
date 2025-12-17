0cba27e1333c62c79a5e473f9bcd1e78aa840ce8
Nilesh Pandey <99646982+NileshPandey05@users.noreply.github.com>
2025-12-17 08:01:48 +0000

    chore: normalize brand ids to sequential order

---
+++ src/app/data/all_bikes.ts
@@ -0,0 +1,19 @@
+import { Prodcuts } from "./data";
+
+interface Allbike {
+    royalenfield: Prodcuts[],
+    bajaj: Prodcuts[]
+    honda: Prodcuts[]
+    tvs: Prodcuts[]
+    yamaha: Prodcuts[]
+}
+
+
+
+export const allbikeslist: Allbike = {
+    royalenfield: [],
+    bajaj: [],
+    honda: [],
+    tvs: [],
+    yamaha: []
+}
---
+++ src/app/data/imageUrls.ts
@@ -0,0 +1,75 @@
+export const baseUrl = "https://raw.githubusercontent.com/NileshPandey05/bikewale/refs/heads/main/bikerwala_assets"
+
+export const royalenfield = `${baseUrl}/bike_img/royalenfield`
+export const tvs = `${baseUrl}/bike_img/tvs`
+export const honda = `${baseUrl}/bike_img/honda`
+export const hero = `${baseUrl}/bike_img/hero`
+export const ktm = `${baseUrl}/bike_img/ktm`
+export const yamaha = `${baseUrl}/bike_img/yamaha`
+export const bajaj = `${baseUrl}/bike_img/bajaj`
+export const suzuki = `${baseUrl}/bike_img/suzuki`
+export const triumph = `${baseUrl}/bike_img/triumph`
+
+//brand logo image url
+
+
+export const amperelogo = `${baseUrl}/bikerwala_assets/brands/ampere.avif`
+export const bmwLogo = `${baseUrl}/bikerwala_assets/brands/bmw.avif`
+export const ducatiLogo = `${baseUrl}/bikerwala_assets/brands/ducati.avif`
+export const heroLogo = `${baseUrl}/bikerwala_assets/brands/hero.avif`
+export const ivoomiLogo = `${baseUrl}/bikerwala_assets/brands/ivoomi.avif`
+export const kineticGreenLogo = `${baseUrl}/bikerwala_assets/brands/kinetic-green.avif`
+export const matterLogo = `${baseUrl}/bikerwala_assets/brands/matter1671004200585.avif`
+export const odysseLogo = `${baseUrl}/bikerwala_assets/brands/odysse.png`
+export const quantumEnergyLogo = `${baseUrl}/bikerwala_assets/brands/quantum-energy.png`
+export const triumphLogo = `${baseUrl}/bikerwala_assets/brands/triumph.png`
+export const vlfLogo = `${baseUrl}/bikerwala_assets/brands/vlf.png`
+
+export const apriliaLogo = `${baseUrl}/bikerwala_assets/brands/aprilia.avif`
+export const bounceLogo = `${baseUrl}/bikerwala_assets/brands/bounce.avif`
+export const evoletLogo = `${baseUrl}/bikerwala_assets/brands/evolet.avif`
+export const hondaLogo = `${baseUrl}/bikerwala_assets/brands/honda.avif`
+export const jawaLogo = `${baseUrl}/bikerwala_assets/brands/jawa.avif`
+export const kineticLogo = `${baseUrl}/bikerwala_assets/brands/kinetic.avif`
+export const motoGuzziLogo = `${baseUrl}/bikerwala_assets/brands/moto-guzzi.avif`
+export const okinawaLogo = `${baseUrl}/bikerwala_assets/brands/okinawa.png`
+export const revoltLogo = `${baseUrl}/bikerwala_assets/brands/revolt.png`
+export const tvsLogo = `${baseUrl}/bikerwala_assets/brands/tvs.avif`
+export const yamahaLogo = `${baseUrl}/bikerwala_assets/brands/yamaha.avif`
+
+export const bajajLogo = `${baseUrl}/bikerwala_assets/brands/bajaj_logo.avif`
+
+export const brixtonLogo = `${baseUrl}/bikerwala_assets/brands/brixton-motorcycles.avif`
+export const ferratoLogo = `${baseUrl}/bikerwala_assets/brands/ferrato.avif`
+export const hopElectricLogo = `${baseUrl}/bikerwala_assets/brands/hop-electric.avif`
+export const joyEBikeLogo = `${baseUrl}/bikerwala_assets/brands/joy-ebike.avif`
+export const ktmLogo = `${baseUrl}/bikerwala_assets/brands/ktm.avif`
+export const motoMoriniLogo = `${baseUrl}/bikerwala_assets/brands/moto-morini.avif`
+export const olaLogo = `${baseUrl}/bikerwala_assets/brands/ola.png`
+export const royalEnfieldLogo = `${baseUrl}/bikerwala_assets/brands/royal-enfield.webp`
+export const ultravioletteLogo = `${baseUrl}/bikerwala_assets/brands/ultraviolette.png`
+export const yezdiLogo = `${baseUrl}/bikerwala_assets/brands/yezdi.png`
+
+export const benelliLogo = `${baseUrl}/bikerwala_assets/brands/benelli.avif`
+export const bsaLogo = `${baseUrl}/bikerwala_assets/brands/bsa.avif`
+export const gemopaiLogo = `${baseUrl}/bikerwala_assets/brands/gemopai.avif`
+export const husqvarnaLogo = `${baseUrl}/bikerwala_assets/brands/husqvarna.avif`
+export const kawasakiLogo = `${baseUrl}/bikerwala_assets/brands/kawasaki.avif`
+export const lambrettaLogo = `${baseUrl}/bikerwala_assets/brands/lambretta.avif`
+export const nortonLogo = `${baseUrl}/bikerwala_assets/brands/norton.avif`
+export const pureEvLogo = `${baseUrl}/bikerwala_assets/brands/pure-ev.png`
+export const simpleEnergyLogo = `${baseUrl}/bikerwala_assets/brands/simple_energy.png`
+export const vespaLogo = `${baseUrl}/bikerwala_assets/brands/vespa.png`
+export const yoLogo = `${baseUrl}/bikerwala_assets/brands/yo.png`
+
+export const bgaussLogo = `${baseUrl}/bikerwala_assets/brands/bgauss.avif`
+export const cfmotoLogo = `${baseUrl}/bikerwala_assets/brands/cfmoto.avif`
+export const harleyDavidsonLogo = `${baseUrl}/bikerwala_assets/brands/harleydavidson.avif`
+export const indianLogo = `${baseUrl}/bikerwala_assets/brands/indian.avif`
+export const keewayLogo = `${baseUrl}/bikerwala_assets/brands/keeway.avif`
+export const lectrixLogo = `${baseUrl}/bikerwala_assets/brands/lectrix.avif`
+export const obenLogo = `${baseUrl}/bikerwala_assets/brands/oben.png`
+export const qjMotorLogo = `${baseUrl}/bikerwala_assets/brands/qj-motor.png`
+export const suzukiLogo = `${baseUrl}/bikerwala_assets/brands/suzuki.avif`
+export const vidaLogo = `${baseUrl}/bikerwala_assets/brands/vida.avif`
+export const zontesLogo = `${baseUrl}/bikerwala_assets/brands/zontes.png`
